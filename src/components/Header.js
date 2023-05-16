c7e4b1f9843614e07aa937f58294fe7daadd1a9e
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-05-15 21:01:04 -0400

    Created Tasks component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import Button from './Button'
 
 const Header = ({name}) => {
+
+    const onClick = () => {
+        console.log('Click')
+    } 
+
     return (
         <header className='header'>
             <h1>Task Tracker</h1>
-            <Button color='green' text='Add'/>
+            <Button onClick={onClick}  color='green' text='Add'/>
         </header>
     )    
 }
