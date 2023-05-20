67aacef87b08940bb5882266fbd8597f5bdbb8a7
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-05-20 14:18:47 -0400

    Json server for backend added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -6,44 +6,41 @@ import AddTask from './components/AddTask'
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
 
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Buy Groceries',
-        day: 'Feb 7th at 2:30pm',
-        reminder: false
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer) 
     }
-])
 
-//Add Task
-const addTask = (task) => {
-  const id = tasks.length + 1
-  const newTask = {id, ...task}
+    getTasks()
+  }, [])
 
-  setTasks([...tasks, newTask])
-}
+  //Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data  = await res.json()
+    return data
+  }
 
-//Delete Task
-const deleteTask = (id) => {
-  setTasks(tasks.filter((task) => task.id !== id))
-}
+  //Add Task
+  const addTask = (task) => {
+    const id = tasks.length + 1
+    const newTask = {id, ...task}
 
-//Toggle Reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder : !task.reminder } : task))
-}
+    setTasks([...tasks, newTask])
+  }
+
+  //Delete Task
+  const deleteTask = async (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder : !task.reminder } : task))
+  }
 
   return (
     <div className='container'>
