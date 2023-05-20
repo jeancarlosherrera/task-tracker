2f69152a8491a600abafd89718c686a7a7a55562
Jean C. Herrera <jeancarlosherreram@gmail.com>
2023-05-20 17:49:07 -0400

    React Router added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import Button from './Button'
+import { useLocation } from 'react-router-dom'
 
 const Header = ({onAdd, showAddTask}) => {
+    const location = useLocation()
 
     return (
         <header className='header'>
             <h1>Task Tracker</h1>
-            <Button onClick={onAdd}  color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} /> 
+            {location.pathname === '/' && <Button onClick={onAdd}  color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} /> }
         </header>
     )    
 }
